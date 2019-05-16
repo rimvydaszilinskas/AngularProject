1aad7839ce428cf20c6628b5ca0f758162bcf9e3
Rimvydas Zilinskas <rimvydas.zilinskas@yahoo.com>
2019-05-16 14:15:11 +0200

    login started, model descriptions in README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatFormField
+} from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule, FormsModule, FormGroup } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group(
+      {
+        username: ['',[Validators.required, Validators.minLength(3)]],
+        password: ['', Validators.required]
+      }
+    );
+  }
+
+}
